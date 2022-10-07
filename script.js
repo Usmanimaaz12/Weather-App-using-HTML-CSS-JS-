8d2fb15ea0c256604f0b94ffe88d27ca2e44cce5
MAAZ USMANI <90022701+Usmanimaaz12@users.noreply.github.com>
2022-10-08 00:21:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ locationBtn.addEventListener("click", () =>{
 });
 
 function requestApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=dadefeed405d35acd1a5879464e9a408`;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=API`;
     fetchData();
 }
 
 function onSuccess(position){
     const {latitude, longitude} = position.coords; // getting lat and lon of the user device from coords obj
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=dadefeed405d35acd1a5879464e9a408`;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=API`;
     fetchData();
 }
 
